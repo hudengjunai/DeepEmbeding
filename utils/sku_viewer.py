48a060d24141f5712e7fd2c220c37f2f596171a1
hudengjun <33210026+hudengjunai@users.noreply.github.com>
2020-03-06 13:08:06 +0800

    Update sku_viewer.py

--- utils/sku_viewer.py
+++ utils/sku_viewer.py
@@ -3,11 +3,11 @@ from pymongo import MongoClient
 from pprint import pprint
 from pymongo import MongoClient
 mongdb={}
-mongdb['host']='dds-bp10da4305cf39f41.mongodb.rds.aliyuncs.com'
+mongdb['host']=''
 mongdb['port']=3717
 client=MongoClient(host=mongdb['host'],port=mongdb['port'])
 dev=client.get_database('dev')
-dev.authenticate(name='nnsearch',password='Eigen123')
+dev.authenticate(name='',password='')
 print(dev.collection_names())
 
 tao_bao_collection = dev.get_collection('image_faiss_dual_taobao')
